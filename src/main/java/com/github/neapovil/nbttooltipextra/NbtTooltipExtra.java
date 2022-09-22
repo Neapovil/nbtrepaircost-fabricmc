0b9c9e8d626122f9ee14f5c8a5c9806fcbef607e
William <nearata@protonmail.com>
2022-09-22 17:18:56 +0200

    init project

---
+++ src/main/java/com/github/neapovil/nbttooltipextra/NbtTooltipExtra.java
@@ -0,0 +1,11 @@
+package com.github.neapovil.nbttooltipextra;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NbtTooltipExtra implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+	}
+}
